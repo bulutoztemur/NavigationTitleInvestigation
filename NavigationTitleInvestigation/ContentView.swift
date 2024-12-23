dedf9adbab3547f8e21dd076c37f17c52462730f
Bulut Oztemur <bulutoztemur@gmail.com>
2024-12-23 19:14:21 +0100

    Initial Commit

---
+++ NavigationTitleInvestigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationTitleInvestigation
+//
+//  Created by bulut.oztemur on 23.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationTitleInvestigation/NavigationTitleInvestigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTitleInvestigationApp.swift
+//  NavigationTitleInvestigation
+//
+//  Created by bulut.oztemur on 23.12.24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTitleInvestigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
